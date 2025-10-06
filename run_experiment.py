c2e588d2ec32070fc4631638a18737dc11b02517
AllenGrahamHart <allenhart094@googlemail.com>
2025-10-06 17:00:38 +0100

    added survey

--- run_experiment.py
+++ run_experiment.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     researcher = ScaffoldedResearcher(
         session_name=f"open_research_Paolo_{datetime.now().strftime('%Y%m%d_%H%M%S')}",
-        max_iterations=4
+        max_iterations=2
     )
 
     researcher.run(problem)
